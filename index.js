456d0df353cb93986327ad8e32bf4dd2cbe2f942
dfavo <favorskyd@gmail.com>
2020-03-13 22:06:01 -0700

    Add new folder public and new files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //(function(exports, require, module, __dirname, __filename ){
+/*
 const chalk = require("chalk");
 const text = require("./data");
 
@@ -6,7 +7,81 @@ console.log(chalk.blue(text));
 
 //console.log(__dirname);
 console.log(__filename);
+*/
 
 //})
 
 // Node JS wraps each module in function
+
+// HTTP allow us to create different Servers
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  /*
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    });
+  } else if (req.url === "/contact") {
+    fs.readFile(path.join(__dirname, "public", "contact.html"), (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    });
+  }
+  */
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  const ext = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch (ext) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    default:
+      contentType = "text/html";
+  }
+
+  if (!ext) {
+    filePath += ".html";
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+    } else {
+      res.writeHead(200, {
+        "Content-Type": contentType
+      });
+      res.end(content);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`);
+});
